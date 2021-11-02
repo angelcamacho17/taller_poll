32e14eaed51697e94017a22f62aa8ee5a89ddb69
JoacoCavarzan <88760680+JoacoCavarzan@users.noreply.github.com>
2021-11-02 11:21:13 -0300

    Add files via upload

---
+++ pollsBK1and2/src/main/java/com/polls/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.polls.repository;
+
+import com.polls.model.Role;
+import com.polls.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
